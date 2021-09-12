e060c16ed8b42a84b493dde2ca0248571cb2f5c4
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2021-09-12 17:45:34 +0530

    Clean architecture + MVVM tutorial complete

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/domain/repository/CoinRepository.kt
@@ -0,0 +1,13 @@
+package com.plcoding.cryptocurrencyappyt.domain.repository
+
+import com.plcoding.cryptocurrencyappyt.data.remote.dto.CoinDetailDto
+import com.plcoding.cryptocurrencyappyt.data.remote.dto.CoinDto
+import com.plcoding.cryptocurrencyappyt.domain.model.Coin
+import com.plcoding.cryptocurrencyappyt.domain.model.CoinDetail
+
+interface CoinRepository {
+
+    suspend fun getCoins() : List<Coin>
+
+    suspend fun getCoinId(coinId: String) : CoinDetail
+}
